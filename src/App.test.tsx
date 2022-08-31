999978f4ebe01b7482b398036779f737db04fb85
qkudev <qkud.ev@gmail.com>
2022-08-31 20:01:53 +0700

    feat(root): adds husky,lint-staged,commitizen

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+
 import App from './App';
+import { store } from './app/store';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
+  const view = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  expect(view.getByText(/learn/i)).toBeInTheDocument();
 });
